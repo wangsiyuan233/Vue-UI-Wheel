504177e392bc40e08915e53fda972c6644997ef5
wangsiyuan233 <380191957@qq.com>
2019-01-27 11:29:22 +0800

    设置了 icon 和 文字 的前后顺序，readme 里 需求 1 和 2

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Vue-UI-Wheel
-造轮子
+# 需求
+**1、 制作 icon :设置 icon 在字体的左边或者右边**
+
+第一步：在 `index.html` 里面 将 `icon-position` 设置为 `right`，`<g-button icon="settings" icon-position="right">设置</g-button>`，剩下的脏活累活都要在 `button.vue` 里面做。
+第二步：在 `button.vue` 里面， 把等高的图标和文字对齐：`display: inline-flex;justify-content: center;align-items: center;`（吃一个 `flex` 三件套）
+第三步：在 `button.vue` 里面，`<button>` 按钮分为 `<svg>` 和 `<div class="content">` 两个部分
+```
+<button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+		<!-- v-if 是用来判断有没有 svg 的，如果没有 svg，字样就是居中的-->
+        <svg class="icon" v-if="icon">
+            <!--第一个 : 是 v-bind 的缩写；反引号表示里面的内容是 js 字符串-->
+            <!--结合 index.html 我们可以知道：用户只用输入 settings ，我们就匹配了 icon-->
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content">
+            <!-- 神奇的地方：控制台里 “设置”两字 在 slot 的位置-->
+            <slot></slot>
+        </div>
+</button>
+```
+配套的 `css` 为
+```
+ /*默认的 icon 位置*/
+        > .icon { order: 1;}
+        > .content {order: 2;}
+
+/*出现 icon-right 时的 icon 位置*/
+        &.icon-right {
+            > .icon {order: 2;}
+            > .content{order:1;}
+        }
+```
+也就是说，在 `index.html` 里面我们一定要设置是好图标是在右边的，剩下的逻辑部分都在 `button.vue` 里面，通过 `svg` 的 `class` 来选择不同的 `order`
+
+2、 发现并排的按钮们在垂直距离上高矮不一
+
+只有一步： `.g-button{ vertical-align: middle;} `
+
+
--- src/button.vue
+++ src/button.vue
@@ -1,52 +1,61 @@
 <!--通过 parcel ，我们知道了 单文件组件-->
-
 <template>
-    <button class="g-button">按钮</button>
-
+    <!--:class="{[iconPosition]:true} 控制 icon 在左边还是在右边-->
+    <button :class="{[`icon-${iconPosition}`]:true}" class="g-button">
+        <!-- v-if 是用来判断有没有 svg 的，如果没有 svg，字样就是居中的-->
+        <svg class="icon" v-if="icon">
+            <!--第一个 : 是 v-bind 的缩写；反引号表示里面的内容是 js 字符串-->
+            <!--结合 index.html 我们可以知道：用户只用输入 settings ，我们就匹配了 icon-->
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content">
+            <!-- 神奇的地方：控制台里 “设置”两字 在 slot 的位置-->
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
-    export default {
-        // name: "button.vue"
-    }
+  export default {
+    props: ['icon', 'iconPosition']
+  }
 
 </script>
 
 <style lang="scss">
-    *{margin:0;padding:0;box-sizing: border-box;}
-    /*root 的兼容性问题 ： 网址caniuse，root 其实就是 html */
-    :root{
-        --botton-height:32px;
-        --front-size:14px;
-        --button-bg: white;
-        --button-active-bg: #eee;
-        --border-radius: 4px;
-        --color: #333;
-        --border-color: #999;
-        --border-color-hover: #666;
-    }
-    #app {
-        margin: 20px;
-    }
-    body {
-        font-size: var(--font-size);
-    }
-
-    .g-button{
-        font-size:var(--front-size);
+    .g-button {
+        font-size: var(--front-size);
         height: var(--botton-height);
         padding: 0 1em;
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+
         &:hover {
             border-color: var(--border-color-hover);
         }
+
         &:active {
             background-color: var(--button-active-bg);
         }
+
         &:focus {
             outline: none;
+        }
+
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+
+        /*默认的 icon 位置*/
+        > .icon { order: 1; margin-right: 0.1em}
+        > .content {order: 2;}
+
+        /*出现 icon-right 时的 icon 位置*/
+        &.icon-right {
+            > .icon {order: 2;margin-right: 0em;margin-left: 0.1em}
+            > .content{order:1;}
         }
     }
 
