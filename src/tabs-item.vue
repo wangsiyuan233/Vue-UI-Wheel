f99e0d09a5dbfe559d1ab865e9e4bb46a1facadd
wangsiyuan233 <380191957@qq.com>
2019-02-06 06:38:14 +0800

    添加部分测试用例

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tabs-item" @click="onClick" :class="classes">
+    <div class="tabs-item" @click="onClick" :class="classes"  :data-name="name">
         <slot></slot>
     </div>
 </template>
@@ -32,15 +32,18 @@
       }
     },
     created () {
-      this.eventBus.$on('update:selected', (name) => {
-        this.active = name === this.name;
-      })
+      if (this.eventBus) {
+        this.eventBus.$on('update:selected', (name) => {
+          this.active = name === this.name;
+        })
+      }
     },
     methods: {
       // click 事件的 xxx
       onClick () {
         if (this.disabled) { return }
-        this.eventBus.$emit('update:selected', this.name, this)
+        this.eventBus && this.eventBus.$emit('update:selected', this.name, this)
+        this.$emit('click', this)
       }
     }
   }
@@ -61,6 +64,7 @@
         }
         &.disabled {
             color: $disabled-text-color;
+            cursor: not-allowed;
         }
     }
 </style>
