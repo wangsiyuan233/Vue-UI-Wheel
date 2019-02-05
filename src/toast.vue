b39641d1479da3260848660c5fd5e059fd44ce0f
wangsiyuan233 <380191957@qq.com>
2019-02-05 08:15:50 +0800

    实现 position: top/bottom/middle 功能

--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="toast" ref="wrapper">
+    <div class="toast" ref="wrapper" :class="toastClasses">
         <div class="message">
             <slot v-if="!enableHtml"></slot>
             <div v-else v-html="$slots.default[0]"></div>
@@ -34,6 +34,13 @@
       enableHtml: {
         type: Boolean,
         default: false
+      },
+      position: {
+        type: String,
+        default: 'top',
+        validator (value) {
+          return ['top', 'bottom', 'middle'].indexOf(value) >= 0
+        }
       }
     },
     created () {
@@ -41,6 +48,13 @@
     mounted () {
       this.updateStyles()
       this.execAutoClose()
+    },
+    computed: {
+      toastClasses () {
+        return {
+          [`position-${this.position}`]: true
+        }
+      }
     },
     methods: {
       updateStyles () {
@@ -77,7 +91,9 @@
     $toast-min-height: 40px;
     $toast-bg: rgba(0, 0, 0, 0.75);
     .toast {
-        font-size: $font-size; min-height: $toast-min-height; line-height: 1.8;        position: fixed; top: 0; left: 50%; transform: translateX(-50%); display: flex;
+        font-size: $font-size; min-height: $toast-min-height; line-height: 1.8;
+        position: fixed; display: flex;
+        left: 50%;
         color: white; align-items: center; background: $toast-bg; border-radius: 4px;
         box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50); padding: 0 16px;
         .message {
@@ -91,6 +107,18 @@
             height: 100%;
             border-left: 1px solid #666;
             margin-left: 16px;
+        }
+        &.position-top{
+            top: 0;
+            transform: translateX(-50%);
+        }
+        &.position-bottom{
+            bottom: 0;
+            transform: translateX(-50%);
+        }
+        &.position-middle{
+            top: 50%;
+            transform: translate(-50%, -50%);
         }
     }
 </style>
